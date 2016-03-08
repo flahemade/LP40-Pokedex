0044c15f3eef7ef04add20b45109609d5db3b062
unknown <felix.lahemade@gmail.com>
2016-03-08 14:29:31 +0100

    Displaying first informations

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+    <html>
+        <head>
+            <title>Pokedex - LP40</title>
+        </head>
+        <body>
+            <h1>Accueil Pokedex</h1>
+            <ul>
+                <li></li>
+            </ul>
+<?php
+ 
+    $pokedex = new DomDocument();
+    $pokedex->load('pokedex.xml');
+?>
+        </body>
+    </html>
